20e4b0ea4cc01ee52bce2e20c5a9efa10701f345
3rd <3rd@users.noreply.github.com>
2024-08-07 03:35:08 +0300

    feat(cli/task): show task count in project sidebar

--- core-cli/ui/task_interactive/components/ProjectsSidebar.go
+++ core-cli/ui/task_interactive/components/ProjectsSidebar.go
@@ -3,6 +3,7 @@ package components
 import (
 	"core/ui/task_interactive/state"
 	"core/ui/task_interactive/theme"
+	"fmt"
 	"strings"
 
 	ui "github.com/3rd/go-futui"
@@ -18,8 +19,9 @@ type ProjectSidebar struct {
 func (c *ProjectSidebar) Render() ui.Buffer {
 	b := ui.Buffer{}
 	b.Resize(c.Width, c.Height)
-	b.FillStyle(ui.Style{Background: theme.PROJECT_SIDEBAR_BG, Foreground: theme.PROJECT_SIDEBAR_FG})
 
+	var entries []string
+	longestTextLength := 0
 	for i := 0; i < c.Height; i++ {
 		projectIndex := i + c.AppState.ProjectScrollOffset
 		if projectIndex >= len(c.AppState.Nodes) {
@@ -27,21 +29,43 @@ func (c *ProjectSidebar) Render() ui.Buffer {
 		}
 		project := c.AppState.Nodes[projectIndex]
 
+		projectName := project.GetName()
+		projectName = strings.TrimPrefix(projectName, "project-")
+		entry := fmt.Sprintf("%s (%d)", projectName, len(project.GetTasks()))
+
+		if len(entry) > longestTextLength {
+			longestTextLength = len(entry)
+		}
+
+		entries = append(entries, entry)
+	}
+
+	width := longestTextLength + 2
+	b.Resize(width, c.Height)
+	b.FillStyle(ui.Style{Background: theme.PROJECT_SIDEBAR_BG, Foreground: theme.PROJECT_SIDEBAR_FG})
+
+	for i := 0; i < c.Height; i++ {
+		if i >= len(entries) {
+			break
+		}
+		entry := entries[i]
 		style := ui.Style{Background: theme.PROJECT_SIDEBAR_BG, Foreground: theme.PROJECT_SIDEBAR_FG}
-		if projectIndex == c.AppState.ProjectSelectedIndex {
+		if i == c.AppState.ProjectSelectedIndex {
 			style.Background = theme.PROJECT_SIDEBAR_SELECTED_BG
 			style.Foreground = theme.PROJECT_SIDEBAR_SELECTED_FG
 		}
 
 		lineBuffer := ui.Buffer{}
-		lineBuffer.Resize(c.Width, 1)
+		lineBuffer.Resize(width, 1)
 		lineBuffer.FillStyle(style)
 		b.DrawBuffer(0, i, lineBuffer)
 
-		projectText := project.GetName()
-		projectText = strings.TrimPrefix(projectText, "project-")
-		b.Text(1, i, projectText, style)
+		text := entry
+		b.Text(1, i, text, style)
 	}
+
+	c.Width = b.Width()
+	c.Height = b.Height()
 
 	return b
 }
